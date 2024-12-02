61c7ef72cd09c835ea47a3f48ce1fcde64072c0d
Yodgorbek <yodgorbekaxmedov0@gmail.com>
2024-12-03 01:11:47 +0500

    modified

--- handlers/ads.py
+++ handlers/ads.py
@@ -4,7 +4,7 @@ from aiogram import F
 
 from router import router
 from loader import db, bot
-from handlers.check_admin_status import check_admin_status
+from handlers.check_admin_status import check_admin_status, ads_supervisor
 
 
 @router.message(F.text == "📰 advertisement")
@@ -14,21 +14,15 @@ async def ads(message: types.Message):
         await message.reply(text="send me the ads\n\n for example: !ads text")
 
 
-@router.message(lambda message: "!ads" in message.text)
+@router.message()
 async def send_ads(message: types.Message):
-    status = await check_admin_status(message.from_user.id)
+    status = await ads_supervisor(message.from_user.id)
     if status:
-        text = message.text.replace("!ads", "", 1)
-        users = db.get_users()
-        for user in users:
-            await bot.send_message(int(user.get("telegram_id")), text)
-
-
-@router.message(F.text == "!test")
-async def delete_last_message(message: types.Message):
-    status = await check_admin_status(message.from_user.id)
-    if status:
-        text = message.text.replace("!test", "", 1)
-        await bot.send_message(message.from_user.id, text)
-        time.sleep(3)
-        await bot.delete_message(chat_id=message.from_user.id, message_id=message.message_id)
+        if len(message.text) >= 10:
+            # users = db.get_users()
+            # for user in users:
+            await bot.copy_message(
+                chat_id=message.from_user.id,
+                from_chat_id=message.from_user.id,
+                message_id=message.message_id
+            )
--- handlers/check_admin_status.py
+++ handlers/check_admin_status.py
@@ -1,6 +1,12 @@
 from loader import bot
+from config import SOURCE_CHANNEL
 
 
 async def check_admin_status(telegram_id):
     membership = await bot.get_chat_member("@english_kinolar_y", telegram_id)
     return membership.status in ['administrator', 'creator']
+
+
+async def ads_supervisor(telegram_id):
+    membership = await bot.get_chat_member(SOURCE_CHANNEL, telegram_id)
+    return membership.status in ['administrator', 'creator', 'owner']
